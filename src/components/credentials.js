2bfedc2450b6c842250f9bca8f70473d1873e072
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2021-06-29 15:22:07 +0530

    deleted

--- src/components/credentials.js
+++
@@ -1,9 +0,0 @@
-const Credentials = () => {
-
-    return {
-        ClientId: 'ba07f5a4785e4320ae51115355fed4e2',
-        ClientSecret: '8af180742cb441069f50df8c256e5086'
-    }
-}
-
-export { Credentials };
