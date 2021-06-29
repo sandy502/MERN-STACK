8079c03d8255733dcc647d364912c68f558a1a0e
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2021-06-29 18:16:45 +0530

    update

--- Project-2/src/components/credentials.js
+++ Project-2/src/components/credentials.js
@@ -1,8 +1,8 @@
 const Credentials = () => {
 
     return {
-        ClientId: 'ba07f5a4785e4320ae51115355fed4e2',
-        ClientSecret: '8af180742cb441069f50df8c256e5086'
+        ClientId: 'your spotify client id',
+        ClientSecret: 'your spotify client secret key'
     }
 }
 
