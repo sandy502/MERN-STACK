52bf3c26f4706e0efe401b16b1adf69888a3c986
Sandaly Singh <56186048+sandy502@users.noreply.github.com>
2021-06-29 15:19:41 +0530

    commit

---
+++ Project-2/src/components/credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'ba07f5a4785e4320ae51115355fed4e2',
+        ClientSecret: '8af180742cb441069f50df8c256e5086'
+    }
+}
+
+export { Credentials };
